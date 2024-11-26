dfff80ae9826da03b43755375f8b09bb7bb81621
JanTomFoerster <jan_tom@f-rster.de>
2024-11-26 21:38:35 +0100

    Simplefied some stuff. Still having the same Problem with the entity Names disapearing when to players meet.

--- src/main/java/GamingLounge/me/eStats/EntityName.java
+++ src/main/java/GamingLounge/me/eStats/EntityName.java
@@ -5,6 +5,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Item;
 
 import net.kyori.adventure.text.Component;
@@ -22,19 +23,23 @@ Bukkit.getScheduler().runTaskTimer(EStats.INSTANCE/*Why the f does this have to 
 
 for (World world : Bukkit.getWorlds()){ //creates the component world which is everry world
     for (Entity activeEntitys : world.getEntities()){//gets the entity's in that world and puts it in the component activeEntitys
-        if (activeEntitys.getPersistentDataContainer().has(ignore)) continue;//checks if it is not supposed to be renamed, by checking for the ignore persistant data container.
-        if (activeEntitys instanceof Item)continue/*skipes this if and goes back to the beginning?*/;
+
+        if (
+            activeEntitys.getPersistentDataContainer().has(ignore)||//checks if it is not supposed to be renamed, by checking for the ignore persistant data container.
+            activeEntitys instanceof Item||//Ignores Item's
+            activeEntitys instanceof FallingBlock//Ignores Falligstuff Like Sand and so on
+            ) continue;
+
         for (Entity player : Bukkit.getOnlinePlayers()){
-            if (player.getLocation().distanceSquared(activeEntitys.getLocation()) <= 10 * 10)/*Checks for entitys surounding the Entity player*/{
+
+            if (player.getWorld()!= activeEntitys.getWorld())continue; //Fixes the plugin checking for the distance between the player and and entity that is not in the same world, by comparing if they are in the same world.
+            if (player.getLocation().distanceSquared(activeEntitys.getLocation()) <= 100){//Checks for entitys surounding the Entity player pls use radius^2. ;)
+                activeEntitys.customName(Component.text("Test"));//Everrything within this can be a component instead of an componant that is the text within this.
                 activeEntitys.setCustomNameVisible(true);//Should activate the name
-                activeEntitys.customName(Component.text("Test")/*Everrything within this can be a component instead of an componant that is the text within this. */);
-/*                if (!(player.getLocation().distanceSquared(activeEntitys.getLocation()) <= 10 * 10)){//Checks if entity is not within this 10*10 range
-                    activeEntitys.setCustomNameVisible(false);//Should deactivate the name
-                } */
-                if(player.getLocation().distanceSquared(activeEntitys.getLocation()) > 10 * 10){/*Checks if the entity is further out then this. */
-                    activeEntitys.setCustomNameVisible(false);//Should deactivate the name.
-                    activeEntitys.remove();//debuging
-                }
+            }
+                
+            else{//If the Entity is further out then it will do the following
+                activeEntitys.setCustomNameVisible(false);//Should deactivate the name.
             }
         }
     }
